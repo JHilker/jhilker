6dd56d66a34363f68ccb8f26ecbef04cfe6f1d4f
Jacob Hilker <hilker.j@gmail.com>
2018-12-20 16:59:06 -0500

    Initial setup

---
+++ bin/jhilker.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+console.log('Name:  Jacob Hilker')
+console.log('Work:  HubSpot');
+console.log('Title: Senior Software Engineer')
+console.log('Team:  Frontend as a Service - Developer Experience')
